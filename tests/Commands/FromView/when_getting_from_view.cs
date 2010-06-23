2d1b62cefc142ac4c87bcb3e6a8e59840d6d97b3
Alex Robson <asrobson@gmail.com>
2010-06-22 22:26:01 -0500

    Adding coverage for view command

---
+++ tests/Commands/FromView/when_getting_from_view.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Machine.Specifications;
+using Relax.Impl.Commands;
+using Relax.Impl.Json;
+
+namespace Relax.Tests.Commands.FromView
+{
+    public class when_getting_from_view : with_get_from_view_command
+    {
+        protected static ViewResult<TestDoc> expectedResult;
+        protected static CommandResult result;
+        protected static TestDoc document;
+
+        private Because of = () =>
+                                 {
+                                     result = command.GetFromView<TestDoc>("test", "test",
+                                                                           x => x.AllowStale().NoReduce());
+                                     expectedResult = result.GetResultAs<ViewResult<TestDoc>>();
+                                     document = expectedResult.Rows.First().Model;
+                                 };
+
+        private It should_only_have_one_record = () => expectedResult.Rows.Length.ShouldEqual(1);
+        private It should_have_correct_id = () => document.DocumentId.ShouldEqual("test");
+        private It should_have_correct_rev = () => document.DocumentRevision.ShouldEqual("1");
+        private It should_have_correct_message = () => document.Message.ShouldEqual("Hi");
+    }
+}
---
+++ tests/Commands/FromView/with_get_from_view_command.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Machine.Specifications;
+using Relax.Impl.Commands;
+using Relax.Impl.Http;
+
+namespace Relax.Tests.Commands.FromView
+{
+    public abstract class with_get_from_view_command : with_command_factory
+    {
+        protected static string url;
+        protected static GetFromViewCommand command;
+        protected static string jsonResult;
+
+        private Establish context = () =>
+                                        {
+                                            jsonResult = @"
+{
+    total_rows: 2,
+    offset: 0,
+    rows:
+        [
+            {
+                doc: { _id: ""test"", _rev: ""1"", Message: ""Hi"" }
+            },
+            {
+                doc: { _id: ""_design/mydesigndoc"" }
+            }
+        ]
+}
+";
+                                            url = @"http://localhost:5984/relax/_design/test/_view/test?stale=ok&reduce=false";
+                                            mockAction
+                                                .Setup(x => x.Get(Moq.It.Is<CouchUri>(u => u.ToString() == url)))
+                                                .Returns(jsonResult);
+
+                                            command = factory.GetViewCommand();
+                                        };
+    }
+}
