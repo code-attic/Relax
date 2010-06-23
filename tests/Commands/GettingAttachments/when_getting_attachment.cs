246c184d6b49adbf74d4a2cb89e7efced9e3e2b3
Alex Robson <asrobson@gmail.com>
2010-06-22 22:36:04 -0500

    Adding tests for get attachment command

---
+++ tests/Commands/GettingAttachments/when_getting_attachment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using Machine.Specifications;
+
+namespace Relax.Tests.Commands.GettingAttachments
+{
+    public class when_getting_attachment : with_get_attachment_command
+    {
+        protected static Tuple<string, byte[]> expectedAttachment;
+        protected static string content;
+
+        private Because of = () =>
+                                 {
+                                     expectedAttachment = command.GetAttachment<TestDoc>("test", "myattachment");
+                                     content = Encoding.UTF8.GetString(expectedAttachment.Item2);
+                                 };
+
+        private It should_have_attachment_name = () => expectedAttachment.Item1.ShouldEqual("myattachment");
+        private It should_have_correct_content = () => content.ShouldEqual("this is a lame attachment :(");
+    }
+}
---
+++ tests/Commands/GettingAttachments/with_get_attachment_command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using Relax.Impl.Commands;
+using Relax.Impl.Http;
+
+namespace Relax.Tests.Commands.GettingAttachments
+{
+    public abstract class with_get_attachment_command : with_command_factory
+    {
+        protected static string url;
+        protected static Tuple<string, byte[]> attachment;
+        protected static GetAttachmentCommand command;
+
+        private Establish context = () =>
+                                        {
+                                            url = @"http://localhost:5984/relax/test/myattachment";
+
+                                            var content = "this is a lame attachment :(";
+                                            var bytes = Encoding.UTF8.GetBytes(content);
+                                            attachment = Tuple.Create("myattachment", bytes);
+
+                                            mockAction
+                                                .Setup(
+                                                    x => x.GetAttachment(Moq.It.Is<CouchUri>(u => u.ToString() == url)))
+                                                .Returns(attachment);
+                                            command = factory.GetGetAttachmentCommand();
+                                        };
+    }
+}
